e8c9c17d0192546f981d6086c479a635037051e4
jtambe <tambe_jayesh@hotmail.com>
2023-09-04 00:18:08 -0700

    Added Dead Letter Queue Handler

---
+++ DeadLetterQueueHandler/Program.cs
@@ -0,0 +1,32 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+using Azure.Messaging.ServiceBus;
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("This is Dead Letter Queue Handler");
+
+// Create a ConfigurationBuilder
+var builder = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+// Build the configuration
+var configuration = builder.Build();
+
+// Retrieve the connection string from the configuration
+string connectionString = configuration.GetConnectionString("PatientOnboardingConnString")!;
+
+
+await using var client = new ServiceBusClient(connectionString);
+
+
+ServiceBusReceiver dlq = client.CreateReceiver("patientonboarding", "PatientIntake", new ServiceBusReceiverOptions { SubQueue = SubQueue.DeadLetter });
+var msg = await dlq.ReceiveMessageAsync();
+
+
+Console.WriteLine(msg.Body.ToString());
+
+await dlq.CompleteMessageAsync(msg);
+
+Console.ReadKey();
+
